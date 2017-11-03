4dda8a68aa52d99736696df82bf9c85e1d49653e
Bharath Gopal <gopalb@codeaurora.org>
2017-11-03 12:29:29 +0530

    santoni: Add property to enable display default color mode
     Add display default color mode property with value of 1.
    
    Change-Id: I89ffc625dbb0fa731dd0d6ed5aa5de4ec97e2f36

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -104,10 +104,11 @@ ro.sf.lcd_density=300 \
 persist.debug.wfd.enable=1 \
 persist.hwc.enable_vds=1 \
 vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=0 \
 vendor.display.rotator_downscale=1 \
 vendor.display.perf_hint_window=50 \
-vendor.gralloc.enable_fb_ubwc=1
+vendor.display.enable_default_color_mode=1 \
+vendor.gralloc.enable_fb_ubwc=1 \
+debug.hwui.use_buffer_age=false
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
