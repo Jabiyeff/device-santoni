0be289c07d4f370fafb5f30a20fd2285bb11ae43
Hua Liu <richardl@codeaurora.org>
2018-01-19 18:34:36 -0800

    santoni: Change scripts for copying MBNs
    1) Using "chmod -h" instead of "chmod"
    2) Using "mkdir -m" instread of 2 seperate commands
    for creating directory and change permission
    3) Using "write" command in RC file instead of "echo ...>"
    in bash
    4) Create "data/vendor/modem_config" for storing MBNs, which
    is read only
    5) Remove qcril.db copying in sh file, since this logic is
    not used anymore

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -353,23 +353,14 @@ fi
 
 cur_version_info=`cat /vendor/firmware_mnt/verinfo/ver_info.txt`
 if [ ! -f /vendor/firmware_mnt/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
-    rm -rf /data/vendor/radio/modem_config
-    mkdir /data/vendor/radio/modem_config
-    chmod 770 /data/vendor/radio/modem_config
-    cp -r /vendor/firmware_mnt/image/modem_pr/mcfg/configs/* /data/vendor/radio/modem_config
-    chown -hR radio.radio /data/vendor/radio/modem_config
-    cp /vendor/firmware_mnt/verinfo/ver_info.txt /data/vendor/radio/ver_info.txt
-    chown radio.radio /data/vendor/radio/ver_info.txt
+    rm -rf /data/vendor/modem_config/*
+    # preserve the read only mode for all subdir and files
+    cp --preserve=m -dr /vendor/firmware_mnt/image/modem_pr/mcfg/configs/* /data/vendor/modem_config
+    cp --preserve=m -d /vendor/firmware_mnt/verinfo/ver_info.txt /data/vendor/modem_config/
+    cp --preserve=m -d /vendor/firmware_mnt/image/modem_pr/mbn_ota.txt /data/vendor/modem_config/
+    chown -hR radio.radio /data/vendor/modem_config/*
 fi
-cp -r /vendor/firmware_mnt/image/modem_pr/mbn_ota.txt /data/vendor/radio/modem_config/mbn_ota.txt
-chown -hR radio.radio /data/vendor/radio/modem_config/mbn_ota.txt
-cp -r /vendor/firmware_mnt/image/modem_pr/mbn_ota1.txt /data/vendor/radio/modem_config/mbn_ota1.txt
-chown -hR radio.radio /data/vendor/radio/modem_config/mbn_ota1.txt
-cp -r /vendor/firmware_mnt/image/modem_pr/mbn_ota2.txt /data/vendor/radio/modem_config/mbn_ota2.txt
-chown -hR radio.radio /data/vendor/radio/modem_config/mbn_ota2.txt
-cp -r /vendor/firmware_mnt/image/modem_pr/mbn_ota3.txt /data/vendor/radio/modem_config/mbn_ota3.txt
-chown -hR radio.radio /data/vendor/radio/modem_config/mbn_ota3.txt
-echo 1 > /data/vendor/radio/copy_complete
+setprop ro.runtime.mbn_copy_completed 1
 
 #check build variant for printk logging
 #current default minimum boot-time-default
