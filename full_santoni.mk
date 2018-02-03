c799fe74fc8f23b915b89a41c7784c8f18316dec
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    santoni: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- full_santoni.mk
+++ full_santoni.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
