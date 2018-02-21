608ac29f3681848bf565db8e0aa2552858ba4ee6
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-21 22:22:40 +0100

    santoni: enable Wi-Fi Aware
    https://source.android.com/devices/tech/connect/wifi-aware
    
    Change-Id: I913f744eb4a41b1d84ac958b8efe06a4408e2182

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -243,6 +243,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_AWARE := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/santoni/BoardConfigVendor.mk
