efac1491172861167a0213e82c79c81eaadaa925
Rajshekar Eashwarappa <reashw@codeaurora.org>
2018-03-14 15:38:59 +0530

    santoni: Build 2.0 Healthd HAL
    Change-Id: Ib14cf2a392cf2def218fbd66d44014bd43e02b03

--- device.mk
+++ device.mk
@@ -211,9 +211,8 @@ PRODUCT_COPY_FILES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
