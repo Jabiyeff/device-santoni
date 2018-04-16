eaa23d77837d616036a62051c7370b67ba131b8f
Vinay Verma <vvinay@codeaurora.org>
2018-04-16 07:56:07 +0530

    santoni: Add MATRIX file for vendor HAL's
    Change-Id: I6c7eed54f066e61ad8039eef5ee6081e6de43560

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,6 +159,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
