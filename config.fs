17d468fd992af48598cef48ab971ae40da16a9c3
Brandon <bsolis@codeaurora.org>
2018-06-05 09:25:27 -0700

    santoni: Update config.fs from CAF
    Change-Id: I45d944376179d0f6f6704563ac3da631a3fa9c64

--- config.fs
+++ config.fs
@@ -101,7 +101,7 @@ caps: WAKE_ALARM
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: WAKE_ALARM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
@@ -133,11 +133,17 @@ user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
 
+[system/vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
 [vendor/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/sensors.qti]
 mode: 0755
