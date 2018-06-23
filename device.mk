eddb68a1133766f73dd688d6a8b9711641daa371
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    santoni: build separate system and vendor hidl libs

--- device.mk
+++ device.mk
@@ -238,9 +238,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_vendor \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_vendor \
+    android.hidl.manager@1.0_system \
     android.hidl.manager@1.0-java
 
 # Input
