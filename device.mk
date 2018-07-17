608389e6863a4cf99828e2c2252cabb928a88cf9
Ritesh Saxena <riteshsax007@gmail.com>
2018-07-17 16:48:07 +0000

    land: Build vendor version of hidl shims
    Change-Id: Ic1a95231564626cde8f5a9f5f6f8bad8cd0159a0

--- device.mk
+++ device.mk
@@ -219,7 +219,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_vendor \
     android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_vendor \
     android.hidl.manager@1.0-java
 
 # Input
