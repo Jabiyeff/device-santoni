276928846cd2d15df11df05a8ceca3c758660fb3
LuK1337 <priv.luk@gmail.com>
2018-08-12 19:40:00 +0000

    santoni: Patchelf gnss blobs
    Change-Id: Ie43d7e867cb71e9282834ae2b1b8f3f10bf1f02e

--- device.mk
+++ device.mk
@@ -390,6 +390,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
