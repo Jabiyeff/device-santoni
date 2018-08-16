42007cb4d95d76838ae4a67e533acf42760b648c
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    santoni: Switch to common basic USB HAL

--- device.mk
+++ device.mk
@@ -419,7 +419,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.santoni
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
