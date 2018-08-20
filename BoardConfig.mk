410934cba8bf9125230eeebc8611ad91a7de39a0
andreisaniuk <fir3walk@gmail.com>
2018-08-20 20:35:18 +0000

    Remove ANT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,9 +47,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := santoni_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
-
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
 AUDIO_FEATURE_ENABLED_ACDB_LICENSE := true
