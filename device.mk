9f74f140d4b90b13a02d6022820574c91976eb30
Han Wang <416810799@qq.com>
2018-10-02 14:00:56 +0200

    santoni: Add textclassifier package for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- device.mk
+++ device.mk
@@ -372,6 +372,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
