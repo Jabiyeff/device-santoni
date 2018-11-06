74ca53a6f15bcdc09323765929f41e45e8b597af
Stefan Berger <s.berger81@gmail.com>
2018-11-06 23:20:50 +0100

    santoni: vendor_prop: Fix screen tearing issue
    Set debug.hwui.use_buffer_age=false to prevent
    screen tearing on scrolling text. Best option for now.
    
    Change-Id: I261157f37f0531d16cb5f28697b5d600ca9e9470

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -110,7 +110,8 @@ vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=0 \
 vendor.display.rotator_downscale=1 \
 vendor.display.perf_hint_window=50 \
-vendor.gralloc.enable_fb_ubwc=1
+vendor.gralloc.enable_fb_ubwc=1 \
+debug.hwui.use_buffer_age=false
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
