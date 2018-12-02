3addec9a1afc8f6744189fec76eb7038f748a232
Ryan Andri <ryanandri@linuxmail.org>
2018-12-02 13:14:22 +0700

    santoni: liblight: sync CAF LA.UM.7.6.r1-03600-89xx.0

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -17,14 +17,15 @@ LOCAL_PATH:= $(call my-dir)
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
+
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_CFLAGS := -DLOG_TAG=\"qdlights\"
 LOCAL_CLANG  := true
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
-LOCAL_HEADER_LIBRARIES := libhardware_headers
-LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
