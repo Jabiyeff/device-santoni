f8bfd485c2f4f96955f8d3c33ed693b95109045d
Ryan Andri <ryanandri@linuxmail.org>
2018-12-03 19:38:49 +0700

    santoni: rootdir: kill gamed service
    * we even don't have gamed bin :/

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -3137,12 +3137,6 @@ case "$target" in
         echo 256 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
         echo 256 > /sys/block/mmcblk0rpmb/queue/read_ahead_kb
         setprop sys.post_boot.parsed 1
-
-        low_ram_enable=`getprop ro.config.low_ram`
-
-        if [ "$low_ram_enable" != "true" ]; then
-        start gamed
-        fi
     ;;
     "msm8974")
         start mpdecision
