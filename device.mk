9c78fa33da5220c9edea33e0742f471ddd5753d5
Ryan Andri <ryanandri@linuxmail.org>
2019-01-04 23:50:34 +0700

    santoni: drop CAF power HAL

--- device.mk
+++ device.mk
@@ -331,11 +331,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.qcom
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.0-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
