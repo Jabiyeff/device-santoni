6953adfe11bacadd39c4b4d0e70276960a38c548
HarukeyUA <inazarrusnak@gmail.com>
2019-01-05 12:34:59 +0000

    Revert "santoni: drop CAF power HAL"
    This reverts commit 9c78fa33da5220c9edea33e0742f471ddd5753d5.

--- device.mk
+++ device.mk
@@ -329,7 +329,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    power.qcom
+
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
