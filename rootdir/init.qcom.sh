49ef91a4ee79abb6a90a042cc7cf351f5c3891c3
HarukeyUA <inazarrusnak@gmail.com>
2019-01-18 08:53:31 +0200

    Remove ims hex

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -399,7 +399,3 @@ case "$buildvariant" in
         ;;
 esac
 
-if [ -f /data/system/users/0/settings_global.xml ]; then
-    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g' /data/system/users/0/settings_global.xml
-    restorecon /data/system/users/0/settings_global.xml
-fi
