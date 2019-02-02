c87279f40384f50613dc01976710cb01fca800ae
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-01 23:33:54 -0200

    santoni: Dirac fixes
    * Pause/play music stream to get effects applied
    * Rename setMusic to setEnabled
    * Increase "BOOT_COMPLETED" broadcast priority

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,7 +33,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DiracUtils.initialize();
+        DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
     }
 }
