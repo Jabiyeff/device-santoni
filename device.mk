b33d353d094f0543a0b6c53b9d024683b2632fe4
HarukeyUA <inazarrusnak@gmail.com>
2019-03-25 21:24:22 +0200

    santoni: Don't build android.hardware.drm@1.0-service.widevine
    We already have blobs in vendor

--- device.mk
+++ device.mk
@@ -190,7 +190,6 @@ PRODUCT_PROPERTY_OVERRIDES += debug.hwui.use_buffer_age=false
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.widevine
 
 # Ebtables
 PRODUCT_PACKAGES += \
