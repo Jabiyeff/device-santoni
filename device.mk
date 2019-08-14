14684697335053a33b6cb8dfc534a5c706a24f41
Bruno Martins <bgcngm@gmail.com>
2019-08-14 16:00:54 +0530

    santoni: Build with VNDK
     * VNDK runtime isolation is kept disabled for now because that's
       also how Xiaomi is shipping P on their devices.

--- device.mk
+++ device.mk
@@ -462,10 +462,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK-SP:
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Wifi
 PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
