6139a2659fd9210547bdd5a8a805aa2a5ee2d552
Stargazer19 <pviraj19@gmail.com>
2019-10-19 23:30:42 +0530

    santoni: Restore Wfdcommon

--- device.mk
+++ device.mk
@@ -455,8 +455,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
-#PRODUCT_BOOT_JARS += \
-#   WfdCommon
+PRODUCT_BOOT_JARS += \
+   WfdCommon
 
 #XiaomiParts
 PRODUCT_PACKAGES += \
