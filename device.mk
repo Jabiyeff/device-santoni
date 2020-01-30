a660d2aa4043e129ca1d5c172cbe7598f8b35da4
Stargazer19 <pviraj19@gmail.com>
2020-01-30 21:31:53 +0530

    santoni: Include Pixel Live Wallpapers package for GApps variant

--- device.mk
+++ device.mk
@@ -502,6 +502,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wallpapers
+ifeq ($(WITH_GAPPS),true)
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
+endif
+
 # Wi-Fi Display
 PRODUCT_BOOT_JARS += \
    WfdCommon
