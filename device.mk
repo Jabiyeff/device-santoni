c6ffd9f5cc08d1236a18c597149a4bd8551aa0ad
JacobDeepu <JacobDeepu@users.noreply.github.com>
2020-02-18 14:03:06 +0530

    land: Build missing libwifi-hal-ctrl

--- device.mk
+++ device.mk
@@ -236,6 +236,9 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service
 
 # GPS
+PRODUCT_PACKAGES += \
+    libwifi-hal-ctrl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
