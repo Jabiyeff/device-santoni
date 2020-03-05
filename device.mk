d2f13dcaed67eb8366944944bb5b19328b626eb8
Stargazer19 <pviraj19@gmail.com>
2020-03-05 10:52:40 +0530

    santoni: build a2dp ldac decoder
    03-05 10:48:15.384 E/a2dp_vendor_ldac_decoder(11707): A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_dec.so: dlopen failed: library "libldacBT_dec.so" not found

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
+	
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libldacBT_dec
 
 # Camera
 PRODUCT_PACKAGES += \
