bc18498961673d3d1b5b215e6c122d084a889877
Stargazer19 <pviraj19@gmail.com>
2020-03-06 17:14:30 +0530

    santoni: Update lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_santoni-userdebug
+    havoc_santoni-userdebug \
+    havoc_santoni-user \
+    havoc_santoni-eng
