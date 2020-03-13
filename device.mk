8776a27c43ae1af20c2ca1ffa961c887545cc426
ZeeLog <Danjel.Zammit@gmail.com>
2020-03-13 14:58:40 +0530

    santoni: biometrics: use prebuilt modified blobs
    * nuke biometrics
    * pin modified fp blobs
    * build android.hardware.biometrics.fingerprint@2.1 from source

--- device.mk
+++ device.mk
@@ -221,7 +221,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.custom
+    android.hardware.biometrics.fingerprint@2.1 \
+    libunwind
 
 # FM
 PRODUCT_PACKAGES += \
