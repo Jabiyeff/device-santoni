52e8c91351e7bfd1a62d5ec8b5c9633ed9b9c8a0
Anton Hansson <>
2020-03-20 11:47:23 +0530

    santoni: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -98,7 +98,6 @@ debug.sf.recomputecrop=0 \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
 ro.qualcomm.svi=0 \
-ro.sf.lcd_density=280 \
 persist.debug.wfd.enable=1 \
 persist.hwc.enable_vds=1 \
 vendor.display.disable_skip_validate=1 \
