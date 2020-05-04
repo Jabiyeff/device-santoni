b4a1d62bdb383aaf5ce9a42bbe339fee0c127f10
Stargazer19 <pviraj19@gmail.com>
2020-05-04 17:54:04 +0530

    [tmp] Nuke Spectrum

--- device.mk
+++ device.mk
@@ -392,8 +392,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.target.rc \
     ueventd.qcom.rc \
-    init.spectrum.rc \
-    init.spectrum.sh
 
 PRODUCT_PACKAGES += \
     init.baseband.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -87,14 +87,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.spectrum.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.spectrum.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.spectrum.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
