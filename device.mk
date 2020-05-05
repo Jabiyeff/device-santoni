3910e23ec2e8d9e413a2784a94e85f593ed4984e
Stargazer19 <pviraj19@gmail.com>
2020-05-05 20:19:45 +0530

    santoni: Import custom powerhal

--- device.mk
+++ device.mk
@@ -363,7 +363,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.1-service.custom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
