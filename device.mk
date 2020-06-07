4f411e5654eef07d48ea02895c170c2447f563f0
Stargazer19 <pviraj19@gmail.com>
2020-06-07 16:31:31 +0530

    santoni: Downrev thermal to 1.0

--- device.mk
+++ device.mk
@@ -469,8 +469,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-impl \
-    android.hardware.thermal@2.0-service
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
