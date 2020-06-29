de44f21474207144a4d43e897bbd68fdf3ea6187
Stargazer19 <pviraj19@gmail.com>
2020-06-29 14:49:09 +0530

    santoni: Remove input device configs for fingerprints
    * Allow fingerprints to wake device always even if no lockscreen is set

--- device.mk
+++ device.mk
@@ -293,10 +293,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_rmi4_i2c.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
-    $(LOCAL_PATH)/idc/gf3208.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/gf3208.idc
-
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
