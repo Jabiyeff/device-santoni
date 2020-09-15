3d6d3a8a4369fd2e1694b1af86a157a6e798d2d2
mathieuchartier <mathieuc@google.com>
2020-09-15 05:53:52 +0530

    santoni: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- device.mk
+++ device.mk
@@ -512,3 +512,6 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
