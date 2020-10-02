b5db202e680043d9a4cb99bd93154b87e7633479
bikram557 <kitkatmukherjee2015@gmail.com>
2020-10-02 20:12:14 +0000

    santoni: Fix offline charging animation

--- rootdir/charger/charger.mk
+++ rootdir/charger/charger.mk
@@ -1,5 +1,5 @@
 PRODUCT_COPY_FILES += \
-    device/xiaomi/santoni/rootdir/charger/chargeonlymode:$(TARGET_OUT_DIR)/root/sbin/charger \
+    device/xiaomi/santoni/rootdir/charger/chargeonlymode:$(TARGET_COPY_OUT_VENDOR)/bin/charger \
     device/xiaomi/santoni/rootdir/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
     device/xiaomi/santoni/rootdir/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
     device/xiaomi/santoni/rootdir/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
