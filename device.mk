b4bc5e551d3a28a59a17897c9e730012cb7f4ca2
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-16 13:08:30 +0400

    santoni: Build libgui_vendor from source
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -174,7 +174,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    libgui_vendor
 
 # Component overrides
 PRODUCT_COPY_FILES += \
