c16cd17322c619ab211692a7dde650955b50402d
Jabiyeff <cebiyevanar@gmail.com>
2020-11-16 09:14:31 +0400

    santoni: Remove duplicate flags
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 # Inherit some common Havoc-OS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
