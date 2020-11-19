6a60fa74a9680270b0c6c8f3d5b4d14bd19f8649
SakilMondal <mondalsakil@gmail.com>
2020-11-19 09:36:12 +0400

    santoni: Build QTI thermal 2.0 hal
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -476,9 +476,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8937
+    android.hardware.thermal@2.0-service.qti
 
 # USB HAL
 PRODUCT_PACKAGES += \
