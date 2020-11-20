1c7f64aea734e03762674845f8737c192455d4e1
Arian <arian.kulmer@web.de>
2020-11-20 23:02:35 +0400

    santoni: camera: Build libdng_sdk.vendor
    Change-Id: Ibeb35612f9e9c6848e7365163b7bb9f7f6944a2c
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -175,6 +175,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
+    libdng_sdk.vendor \
     libgui_vendor
 
 # Component overrides
