da647cdf2ec88842e41f418f537488fed7932867
Jabiyeff <cebiyevanar@gmail.com>
2020-12-04 00:13:27 +0400

    Revert "santoni: Compile HWUI for Better Performance"
    This reverts commit 752e1f6f53dd0d343db12892bb588ab81093d8a7.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,9 +182,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
-# HWUI
-HWUI_COMPILE_FOR_PERF := true
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_santoni
 TARGET_RECOVERY_DEVICE_MODULES := libinit_santoni
