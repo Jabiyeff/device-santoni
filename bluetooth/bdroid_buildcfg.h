19b389649fec4c66699ac9fe3cb218635c99df22
Nolen Johnson <johnsonnolen@gmail.com>
2020-12-03 09:09:17 +0400

    santoni: bluetooth: Disable clean turn on
    * This isn't needed anymore.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Change-Id: Ib77e5212099d82bbc07aea6044eb35109faff5cb

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,7 +24,6 @@
 #define BLE_VND_INCLUDED   TRUE
 
 // skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED TRUE
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #define BTM_WBS_INCLUDED TRUE
