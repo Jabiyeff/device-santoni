1c13a67f46d32053f167456e4abc8cf66eb86963
Bruno Martins <bgcngm@gmail.com>
2020-12-12 21:27:31 +0400

    santoni: Add custom Touch HAL
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -481,6 +481,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8937
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.santoni
