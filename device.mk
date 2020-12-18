b5a53b535208a361504d365ac68ec31bae96d07f
Joey <joey@lineageos.org>
2020-12-18 13:35:23 +0400

    santoni: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -489,6 +489,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_8937
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.santoni
