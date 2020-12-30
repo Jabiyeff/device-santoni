5ec49ef657dab3949bdac4b2cf286cc6df34df15
Ethan Chen <intervigil@gmail.com>
2020-12-30 22:26:43 +0400

    santoni: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -361,12 +361,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Netutils
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0 \
-    android.system.net.netd@1.0 \
-    libandroid_net
-
 # OMX
 PRODUCT_PACKAGES += \
     libmm-omxcore \
