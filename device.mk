5f35c2d530310a1e0c0f610e502e840cac101598
Jabiyeff <cebiyevanar@gmail.com>
2021-01-06 13:01:01 +0400

    santoni: libhidl: Add hidl manager target

--- device.mk
+++ device.mk
@@ -286,6 +286,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java \
     libhidltransport \
     libhidltransport.vendor
