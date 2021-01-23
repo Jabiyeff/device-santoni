e9cd18c176829dbe962afa5c8377f64e8c479707
Arne Coucheron <arco68@gmail.com>
2021-01-23 11:35:56 +0400

    santoni: Use in-process APK for network stack and tethering
    Taken from common go configuration, and the purpose is
    to save some memory.
    
    Change-Id: I32222daf8fa2be1f41a8177b82fa6f2f94e002e7
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -553,6 +553,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# Do not spin up a separate process for the network stack, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
