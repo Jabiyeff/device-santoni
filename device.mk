4492e94e65d0723b80fda3b567d3d9d04f1ee5af
Jabiyeff <cebiyevanar@gmail.com>
2021-02-06 13:26:41 +0400

    santoni: Build new sensor blobs
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -476,6 +476,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1 \
     libsensorndkbridge
 
 # Shims
