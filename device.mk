f5232258bd2023527f002e0550fc19f517f12f46
Jabiyeff <cebiyevanar@gmail.com>
2021-03-27 20:27:07 +0400

    santoni: Restore back perf blobs from LA.UM.8.6.2.r1-08600-89xx.0
    * LOS 18.1 Power Profiles doesn't work on latest perf 2.2
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -359,9 +359,9 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@1.0.vendor \
     vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
