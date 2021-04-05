fd720e2e296a995193c5d67a842090c81632dac9
Jabiyeff <cebiyevanar@gmail.com>
2021-04-05 14:02:11 +0400

    Revert "santoni: light: Convert lights HAL into a native binderized HAL"
    This reverts commit 899551de5596bbe6a9ed28f10befb72cde74e62c e785414bb068e938813faf8fdd0e44079f0e0663 4ba5b81df1d5605aa0e19857d2683789c7406acf 3d7f779adaf97c756016d645f1dcfdb53eff76da 6acf2b9ec84a43eb3d339a573f9bf21db16d8f8b e9340e072a5f618c302bf216478dbe2e783a8fcd 2cd11bc40cc5d0f8dbff1e2367445a3e0c7ee63d 6b72d51ca56c4adcdc9161639a68a4040adee750 c674436d0d75ae5d6091a205fdeabecdf23e0e05 ab1b4f62e851881e2c173fc7b2b5d1f238e477b1.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,6 +179,9 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_santoni
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # Malloc
 MALLOC_SVELTE_FOR_LIBC32 := true
 
--- device.mk
+++ device.mk
@@ -312,7 +312,11 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.santoni
+    lights.msm8937
+
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media
 PRODUCT_COPY_FILES += \
