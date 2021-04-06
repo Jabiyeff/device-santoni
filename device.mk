4a0452fe4681b725c8b97108ecaab6fe7ad9930e
Bruno Martins <bgcngm@gmail.com>
2021-04-06 21:01:35 +0400

    santoni: Build LiveDisplay 2.0 HAL
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -315,6 +315,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
