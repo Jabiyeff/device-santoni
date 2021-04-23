adb8be9bd3d1796098c7ac5b7a31e0babb719ad9
Mahesh Sharma <smahesh@codeaurora.org>
2021-04-23 23:37:22 +0400

    santoni: ANT: Support new ANT stack
    Add appropriate package name on the basis of
    transport used on target.
    
    Change-Id: I02163a7a50afba8c6ee6a766327b78c5df081d4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -89,9 +89,9 @@ PRODUCT_COPY_FILES += \
 
 # ANT
 PRODUCT_PACKAGES += \
-    AntHalService \
+    AntHalService-Soong \
     com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant@1.0.vendor
 
 # Audio
 PRODUCT_PACKAGES += \
