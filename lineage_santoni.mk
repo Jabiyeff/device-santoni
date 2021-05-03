a9009ffa2102ced8b30b140e60558de437651798
Jabiyeff <cebiyevanar@gmail.com>
2021-05-03 21:33:02 +0400

    santoni: Include prebuilt apps repository
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -25,6 +25,9 @@ $(call inherit-product, device/xiaomi/santoni/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Prebuilt apps
+$(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
+
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
