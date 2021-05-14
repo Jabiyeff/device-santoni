9c251e9c0afbc49aadb931e22101e60546a88bfe
Jabiyeff <cebiyevanar@gmail.com>
2021-05-14 23:21:45 +0400

    santoni: Import HavocOS support
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
+# LineageOS (crDroid)
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_santoni.mk
 
@@ -5,3 +6,12 @@ COMMON_LUNCH_CHOICES := \
     lineage_santoni-userdebug \
     lineage_santoni-user \
     lineage_santoni-eng
+
+# HavocOS
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/havoc_santoni.mk
+
+COMMON_LUNCH_CHOICES += \
+    havoc_santoni-userdebug \
+    havoc_santoni-user \
+    havoc_santoni-eng
