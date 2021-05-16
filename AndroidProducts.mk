1bb84e2d7c9c0205841e40128bc9be0b81ab23c0
Jabiyeff <cebiyevanar@gmail.com>
2021-05-16 11:57:07 +0400

    santoni: Import AOSiP support
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,3 +15,12 @@ COMMON_LUNCH_CHOICES += \
     havoc_santoni-userdebug \
     havoc_santoni-user \
     havoc_santoni-eng
+
+# AOSiP
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/aosip_santoni.mk
+
+COMMON_LUNCH_CHOICES += \
+    aosip_santoni-userdebug \
+    aosip_santoni-user \
+    aosip_santoni-eng
---
+++ aosip_santoni.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2019-2021 The AOSiP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit from santoni device
+$(call inherit-product, device/xiaomi/santoni/device.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := santoni
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 4X
+PRODUCT_NAME := aosip_santoni
+BOARD_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
+	
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys"
