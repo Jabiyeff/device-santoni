a0c214f0d76ee8f540d410446a957f2f91de87a1
Jabiyeff <cebiyevanar@gmail.com>
2021-05-26 19:11:09 +0400

    santoni: Only support crDroid(LOS) here
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,21 +6,3 @@ COMMON_LUNCH_CHOICES := \
     lineage_santoni-userdebug \
     lineage_santoni-user \
     lineage_santoni-eng
-
-# HavocOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/havoc_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
-
-# AOSiP
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosip_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosip_santoni-userdebug \
-    aosip_santoni-user \
-    aosip_santoni-eng
