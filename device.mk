a1e690af3fa9dc4745ec93421983d342730c4d28
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-31 22:06:19 +0400

    santoni: Build custom lights AIDL
    Change-Id: Id7d8c2c5c141dba8006eb6e800dcd8777e1441d6
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -300,11 +300,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8937
-
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.lights-service.santoni
 
 # Media
 PRODUCT_COPY_FILES += \
