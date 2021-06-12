2f4f816cad4aec545f97cd5c1d868c0ece71d458
Jabiyeff <cebiyevanar@gmail.com>
2021-06-12 12:41:44 +0400

    santoni: Switch thermal-engine from Olive V12.0.3.0
    * Also, restore Thermal HAL 1.0
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -430,6 +430,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/santoni
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl:64 \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
