825bf58877260f429d476cda35266e63cf35e04a
ZeeLog <Danjel.Zammit@gmail.com>
2021-06-14 23:31:37 +0400

    santoni: Remove unused QTI BT audio blobs
    * move BT blobs to proper section
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -136,8 +136,6 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
