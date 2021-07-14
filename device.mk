4a444fc27c379fb767a308708ce44b7faa236af6
Jabiyeff <cebiyevanar@gmail.com>
2021-07-15 00:59:00 +0400

    santoni: Remove custom PRODUCT_SYSTEM_SERVER_COMPILER_FILTER value
    * Sometimes it create build error on dex2oat
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -493,9 +493,6 @@ PRODUCT_PACKAGES += com.android.tethering.inprocess
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
