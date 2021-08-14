fe589e5ded8894a2c68da1db802e699accc9a541
Jabiyeff <cebiyevanar@gmail.com>
2021-08-14 23:36:26 +0400

    santoni: init: Optimize dalvik values from 2GB RAM model
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- init/init_santoni.cpp
+++ init/init_santoni.cpp
@@ -39,6 +39,7 @@
 #include "log/log.h"
 
 char const *heapgrowthlimit;
+char const *heapsize;
 char const *heaptargetutilization;
 char const *heapminfree;
 char const *heapmaxfree;
@@ -52,18 +53,21 @@ void check_device()
     if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xhdpi-4096-dalvik-heap.mk // increased heapgrowthlimit
         heapgrowthlimit = "256m";
+        heapsize = "512m";
         heaptargetutilization = "0.6";
         heapminfree = "8m";
         heapmaxfree = "16m";
     } else if (sys.totalram > 2048ull * 1024 * 1024) {
         // from - custom (adapted 2048-4096 values)
         heapgrowthlimit = "256m";
+        heapsize = "512m";
         heaptargetutilization = "0.75";
         heapminfree = "2m";
         heapmaxfree = "8m";
     } else {
         // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapgrowthlimit = "192m";
+        heapgrowthlimit = "128m";
+        heapsize = "256m";
         heaptargetutilization = "0.75";
         heapminfree = "512k";
         heapmaxfree = "8m";
@@ -100,7 +104,7 @@ void vendor_load_properties()
 
     property_override("dalvik.vm.heapstartsize", "8m");
     property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapsize", heapsize);
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
