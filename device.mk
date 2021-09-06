9102f8ba355b03c725b039506d6a55967c66f398
Jabiyeff <cebiyevanar@gmail.com>
2021-09-06 12:45:21 +0400

    Revert "santoni: Nuke FM Radio"
    This reverts commit d7f4f39d1c7037c302825af88b7acc966e6c8a3e.

--- device.mk
+++ device.mk
@@ -217,6 +217,11 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.2-service.custom \
     fakelogprint
 
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    libfmjni
+
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
