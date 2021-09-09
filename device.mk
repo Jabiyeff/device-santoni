720f39d38fd9b70e672106897994efa4e8b76bb9
sb6596 <shubhamprince111@gmail.com>
2021-09-09 23:17:49 +0400

    santoni: Downgrade SoundTrigger to 2.1
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl:32 \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
+    android.hardware.soundtrigger@2.1-impl:32 \
     android.hardware.broadcastradio@1.0-impl
 
 # Audio configuration
