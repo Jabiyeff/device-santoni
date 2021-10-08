2450cc4a54490f3cf12ba98a52bcb4397d81f321
Jabiyeff <cebiyevanar@gmail.com>
2021-10-09 00:51:05 +0400

    (TMP) santoni: Remove XiaomiParts
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -486,10 +486,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     libnl
 
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Do not spin up a separate process for the network stack, use an in-process APK.
 PRODUCT_PACKAGES += InProcessNetworkStack
 PRODUCT_PACKAGES += com.android.tethering.inprocess
