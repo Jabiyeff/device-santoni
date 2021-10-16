9d61d5189d4228d8eb8e03c195bb66f61a353fbd
Jabiyeff <cebiyevanar@gmail.com>
2021-10-16 16:12:17 +0400

    antoni: Add AEX 12.0 support
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,12 @@ COMMON_LUNCH_CHOICES := \
     arrow_santoni-userdebug \
     arrow_santoni-user \
     arrow_santoni-eng
+
+# AEX
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/aosp_santoni.mk
+
+COMMON_LUNCH_CHOICES += \
+    aosp_santoni-userdebug \
+    aosp_santoni-user \
+    aosp_santoni-eng
