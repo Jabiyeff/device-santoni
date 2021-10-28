2c01165f28922c115882738390d5f28be5a3ad7d
Mikhail Naganov <mnaganov@google.com>
2021-10-28 11:24:27 +0400

    santoni: switch audio HAL to V7 Updated audio policy manager configuration files to comply with the changes in the XML schema.
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I55d3728c809d27e58d5d496a1c7d637050dde53a
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -101,13 +101,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.audio@6.0 \
-    android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio.common@6.0 \
-    android.hardware.audio.common@6.0-util \
+    android.hardware.audio@7.0 \
+    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio.common@7.0 \
+    android.hardware.audio.common@7.0-util \
     android.hardware.audio.effect@2.0-impl:32 \
-    android.hardware.audio.effect@6.0 \
-    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio.effect@7.0 \
+    android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
     android.hardware.broadcastradio@1.0-impl
 
