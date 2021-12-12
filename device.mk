b9269ab4671cf631132a1a396207009c30bef1d1
Chenyang Zhong <zhongcy95@gmail.com>
2021-12-12 12:50:17 +0400

    santoni: Ship cgroups.json and task_profiles.json in vendor Google's prebuilt cgroups.json and task_profiles.json for products launched with previous API levels only covers ro.product.first_api_level >= 28. [1]
    Santoni was launched with Nougat (API level 25), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Change-Id: I7d7726f2c887fdc607c6bcede288e36681073fd9
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -168,6 +168,11 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     libgui_vendor
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
