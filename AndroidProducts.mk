ad325c07668635b6e756037e7c7e3e754e6516cb
Jabiyeff <cebiyevanar@gmail.com>
2022-08-23 12:47:06 +0400

    santoni: ArrowOS 13.0 bring up
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 # LineageOS (crDroid)
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/arrow_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    arrow_santoni-userdebug \
+    arrow_santoni-user \
+    arrow_santoni-eng
